e7c46ed40b47232aa6752d61b032bfce3643ea29
Roussieau <julian.roussieau@student.uclouvain.be>
2017-04-25 20:00:55 +0200

    delete delay

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ ln -svf $current/vim/vimrc ~/.vimrc
 ln -svf $current/tmux/tmux.conf ~/.tmux.conf
 ln -svf $current/zsh/zshrc ~/.zshrc
 ln -svf $current/git/gitconfig ~/.gitconfig
-ln -svf $current/vim/vim ~/.vim
+ln -svf $current/vim/vim/* ~/.vim
