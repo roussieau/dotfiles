ab0d51498e0f91ca50a584c4c3703e54f7a58240
Roussieau <julian.roussieau@student.uclouvain.be>
2019-09-14 22:17:33 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,46 @@
-#!/bin/zsh
+#!/bin/sh
 current=~/.dotfiles
 
-ln -svf $current/vim/vimrc ~/.vimrc
-ln -svf $current/tmux/tmux.conf ~/.tmux.conf
-ln -svf $current/zsh/zshrc ~/.zshrc
-ln -svf $current/git/gitconfig ~/.gitconfig
-ln -svf $current/vim/vim/* ~/.vim
+#Homebrew
+echo 'Installing homebrew'
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Updating brew
+echo 'Updating brew...'
+brew update 
+brew=(
+  'git'
+	'vim'
+	'zsh'
+)
+
+for software in "${brew[@]}"; do
+    echo "Installation of $software..."
+    brew install $software
+done
+
+#brew cask
+brew_cask=(
+  'sublime-text'
+  'vlc'
+	'google-chrome'
+	'spotify'
+)
+
+for software in "${brew_cask[@]}"; do
+    echo "Installation of $software..."
+    brew cask install $software
+done
+
+###Setup zsh
+#echo 'Setup zsh'
+#chsh -s /bin/zsh
+#echo 'Installing Oh my zsh'
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#
+#
+#ln -svf $current/vim/vimrc ~/.vimrc
+#ln -svf $current/tmux/tmux.conf ~/.tmux.conf
+#ln -svf $current/zsh/zshrc ~/.zshrc
+#ln -svf $current/git/gitconfig ~/.gitconfig
+#ln -svf $current/vim/vim/* ~/.vim
